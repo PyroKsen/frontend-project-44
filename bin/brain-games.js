232a82e469430629d6134505ccbbff42046b7eb2
PyroKsen <maxmax11219@yandex.ru>
2024-01-16 16:57:10 +0300

    sss

---
+++ bin/brain-games.js
@@ -0,0 +1,3 @@
+import { hi } from '../src/cli.js';
+
+const result = hi()
---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+export const hi = () => {
+
+console.log('Welcome to the Brain Games!');
+
+const name = readlineSync.question('May I have your name?: ');
+
+console.log('Hello, ' + name);
+
+}
